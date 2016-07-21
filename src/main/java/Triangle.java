7ad33b7d49fba8cdd6a3035c6915ac9a9d71c681
Sasa33 <chengsasa33@gmail.com>
2016-07-21 08:33:49 +0800

    sasa: write first test case and implement it

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,11 @@
+public class Triangle {
+    private int num;
+
+    public Triangle(int n) {
+        this.num = n;
+    }
+
+    public void print() {
+        System.out.print('*');
+    }
+}
---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,39 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TriangleTest {
+    Triangle triangle;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    @Test
+    public void should_print_one_star_when_input_number_is_one() {
+        triangle = new Triangle(1);
+
+        triangle.print();
+
+        String expectedOutput  = "*";
+
+        assertEquals(outContent.toString(), expectedOutput);
+    }
+}
