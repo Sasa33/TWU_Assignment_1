b2ed58591d6247d68277e88e2f761092cd460913
Sasa33 <chengsasa33@gmail.com>
2016-07-21 18:25:47 +0800

    add first test case for factorization

---
+++ src/main/java/Factorization.java
@@ -0,0 +1,11 @@
+public class Factorization {
+    private int number;
+
+    public Factorization(int i) {
+        this.number = i;
+    }
+
+    public void generate() {
+        System.out.print("");
+    }
+}
